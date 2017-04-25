66996fa210efcb4695f2071695586bc48daa0242
fin <fin+github@xbhd.org>
2017-04-25 16:02:08 +0200

    change things up for 2017 election results

---
+++ frenchy/Result.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import math
+
+class Result():
+    def __init__(self, url):
+        self.url = url
+        tables = pd.read_html(self.url, header=0, encoding='utf8', decimal=',',
+                              thousands=' ')
+        self._parse([x for x in tables if not x.empty])
+
+    def _parse(self, tables):
+        rounds = math.floor(len(tables)/2)
+        #results_round1 = self._parse_results(tables[3], 1)
+        #results_round2 = self._parse_results(tables[1], 2)
+        self.results = pd.concat([self._parse_results(t, rounds-i) for i,t in enumerate(tables[0::2])])
+
+        #meta_round1 = self._parse_meta(tables[2], 1)
+        #meta_round2 = self._parse_meta(tables[0], 2)
+        #self.meta = pd.concat([meta_round1, meta_round2])
+        self.meta = pd.concat([self._parse_meta(t, rounds-i) for i,t in enumerate(tables[1::2])])
+
+    def _parse_results(self, table, round_):
+        results_cols = {'Liste des candidats ': 'candidate', 'Voix': 'votes'}
+        table = table.rename(columns=results_cols)
+        table['candidate'] = table['candidate'].str.replace('\xa0', ' ')
+        table['round'] = round_
+        table = table[['round', 'candidate', 'votes']]
+        table = table.set_index(['candidate', 'round'])
+        return table
+
+    def _parse_meta(self, table, round_):
+        clean = lambda x: int(str(x).replace('\xa0', ''))
+        table = table[['Unnamed: 0', 'Nombre']].iloc[0:4].set_index('Unnamed: 0').T
+        meta_cols = {'Inscrits': 'registered', 'Votants': 'votes',
+                     'Blancs ou nuls': 'blanks_or_nulls',
+                     'Blancs': 'blanks_or_nulls',
+                     'Abstentions': 'abstentions'}
+        table = table.rename(columns=meta_cols)
+        table['round'] = round_
+        table = table.set_index('round')
+        table.columns.name = None
+        table['registered'] = table['registered'].apply(clean)
+        table['votes'] = table['votes'].apply(clean)
+        table['blanks_or_nulls'] = table['blanks_or_nulls'].apply(clean)
+        table['abstentions'] = table['abstentions'].apply(clean)
+
+        return table
--- frenchy/__init__.py
+++ frenchy/__init__.py
@@ -1,20 +1,20 @@
-from .Commune import Commune
-from .utils import df
+from .Result import Result
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
 
+def get_departement_results(base_url):
+    s = BeautifulSoup(requests.get(base_url).content)
+    departements = [(x['value'], x.text,) for x in s.select('select option[value]') if x['value']!='#']
+    results = {}
+    for url,name in departements:
+        r = get(base_url.replace('index.html', url))
+        r.meta['departement']=name
+        r.results['departement']=name
+        results[name] = r
+    return {'results': pd.concat([x.results for x in results.values()]),
+            'meta': pd.concat([x.meta for x in results.values()]),
+            }
 
-def list_regions():
-    return df[['region', 'region_code']].drop_duplicates()
-
-
-def list_departments():
-    return df[['department', 'department_code',
-               'region_code']].drop_duplicates()
-
-
-def list_communes():
-    return df[['commune', 'commune_code', 'insee_code',
-               'department_code', 'region_code']].drop_duplicates()
-
-
-def get(code, year):
-    return Commune(code, year)
+def get(url):
+    return Result(url)
